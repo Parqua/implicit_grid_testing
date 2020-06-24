40aba28826e08ce73e73a1dd4f3231e46d857519
Kae Suarez <suarez29@llnl.gov>
2020-06-24 08:40:26 -0700

    Two-d triangle generation

--- src/Grid_Storage.hpp
+++ src/Grid_Storage.hpp
@@ -17,6 +17,7 @@ using ImpGrid3d = axom::spin::ImplicitGrid_Accel<in3D>;
 using BBox3d = axom::primal::BoundingBox<double, in3D>;
 using Tri3d = axom::primal::Triangle<double, in3D>;
 using Point3d = axom::primal::Point<double, in3D>;
+using namespace axom::utilities;
 
 class GridStorage {
     public:
@@ -28,7 +29,7 @@ class GridStorage {
                 double eytents[2] = {bmax, bmax};
                 int real_res[2] = {cellcount, cellcount};
                 two_stor = ImpGrid2d(extents, eytents, real_res, objcount);
-
+                genTriangles2d(objcount, bmax);
             }
             else if(dim == 3){
                 double extents[3] = {bmax, bmax, bmax};
@@ -39,8 +40,25 @@ class GridStorage {
 
         }
 
+    private:
 
+        void genTriangles2d(int tricount, double bmax){
+        Point2d p[3];
+            double size_per_tri = bmax/(tricount*2);
+            for(int i = 0; i < tricount/2; i++){
+                for(int j= 0; j < tricount/2; j++){
+                    p[0] = Point2d::make_point(random_real(i*size_per_tri, (i*size_per_tri+1)),random_real(j*size_per_tri,j*(size_per_tri+1)));
+                    p[1] = Point2d::make_point(random_real(i*size_per_tri, (i*size_per_tri+1)),random_real(j*size_per_tri,j*(size_per_tri+1))); 
+                    p[2] = Point2d::make_point(random_real(i*size_per_tri, (i*size_per_tri+1)),random_real(j*size_per_tri,j*(size_per_tri+1))); 
+                    Tri2d t(p[0], p[1], p[2]);
+                    two_tstor.push_back(t);
+                }
+            }
+        }
 
+        void genTriangles3d(int tricount, double bmax){
+
+        }
     private:
         ImpGrid2d two_stor;
         ImpGrid3d three_stor;
